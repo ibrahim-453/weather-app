a8f0b83dc0c0292d8fdae77b83ca85363276c1be
ibrahim-453 <ranaibrahim453@gmail.com>
2025-08-06 00:43:14 +0500

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header"
 import HeroSection from "./components/HeroSection"
+import { WeatherProvider } from "./context/WeatherContext"
 function App() {
   return (
     <div>
+      <WeatherProvider>
       <Header />
       <HeroSection />
+      </WeatherProvider>
     </div>
   )
 }
